5abee77efa4830a5e41a240b755bd27d6a13b8d7
kammitama5 <krystal.maughan@gmail.com>
2016-10-24 18:58:34 -0700

    1st commit

---
+++ _002.groovy
@@ -0,0 +1,9 @@
+def x = "Krystal"
+println "Hellooo, $x"​​​​​​​​​​​​​​​​
+
+def firstName = "Krystal"
+def name = "Wonder"
+println "Hello, ${firstName[0]}​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​. $name"​​​​​​​​​
+
+//prints Hellooo, Krystal
+//prints Hello, K. Wonder​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
