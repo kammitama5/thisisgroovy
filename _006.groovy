0c05cd6000d2ca261e869366b4617cbde65190a6
kammitama5 <krystal.maughan@gmail.com>
2016-10-24 19:15:08 -0700

    1st commit: Closures

---
+++ _006.groovy
@@ -0,0 +1,17 @@
+//Clojures 
+def mult = { int x, int y ->
+
+    return x * y
+
+
+}
+
+println mult(2, 3)​ //prints 6
+println mult(22, 5) // prints 110
+
+def sqr = { int a ->
+    return a * a
+}
+
+println sqr(3) // prints 9
+println sqr(100)​ //prints 10000
